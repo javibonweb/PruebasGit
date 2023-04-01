af56599c71498fdb3a702b38299975aba809d4ad
javibonano <129430960+javibonano@users.noreply.github.com>
2023-04-01 17:15:34 +0200

    Comienzo de proyecto.

---
+++ src/EjemploGit.java
@@ -0,0 +1,9 @@
+
+public class EjemploGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hola amigo, un saludo desde GIT con FJBR");
+	}
+
+}
