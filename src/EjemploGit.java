bb1dd7042c1774374093ffda9df7b09875ee6216
javibonano <129430960+javibonano@users.noreply.github.com>
2023-04-01 17:30:58 +0200

    Agregada variable nombre

--- src/EjemploGit.java
+++ src/EjemploGit.java
@@ -4,6 +4,8 @@ public class EjemploGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hola amigo, un saludo desde GIT con FJBR");
+		
+		String nombre = "Javi Bonano";
 	}
 
 }
